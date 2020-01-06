0ce6ce6413dec912d250497cf5e0796e3d35f889
Haluskua <haluskua.one@gmail.com>
2020-01-06 22:23:55 +1300

    managing state with context API with movieContext to pass state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
-import MovieList from './MovieList';
-import Nav from './Nav';
+import MovieList from "./MovieList";
+import Nav from "./Nav";
+import { MovieProvider } from "./MovieContext";
 
 function App() {
-  return <div className="App">
-    <Nav />
-    <MovieList/>
-  </div>;
+  return(
+  <MovieProvider>
+    <div className="App">
+      <Nav />
+      <MovieList />
+    </div>
+  </MovieProvider>
+  );
 }
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MovieContext } from './MovieContext';
+
 const Nav = (props) => {
+    const [movies, setMovies] = useContext(MovieContext);
  
   return (
-    <>
+    <div className="nav">
       <h3>Dev Ompa</h3>
-      <p>List of Movies: </p>
+      <p>List of Movies: {movies.length}</p>
 
-    </>
+    </div>
   );
 };
 
